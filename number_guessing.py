9a70ae5553cb83fa4d0529092919007bd2912502
Loges2003 <loges23@gmail.com>
2020-12-21 11:42:32 +0530

    updated with random number generation

--- number_guessing.py
+++ number_guessing.py
@@ -1,25 +1,29 @@
 #https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/
 # Ex:2 Number Guessing
-
+import random
 print("Guess a number between 1- 100 in 5 guesses")
 
-a = 45
+#a = 45
+a = random.randint(1,100)
 
 for i in range(5):
 
     num = int(input("Enter the number:"))
+    diff = num-a
+
 
     if num == a:
         print('Great, You win!!!')
         break
 
-    elif num <= 40:
+    elif diff <= -5:
         print('Your Guess too less than the hidden number')
 
-    elif num >= 50:
+    elif diff >= 5:
         print('Your Guess is too far than hidden number')
 
     else:
         print('you were almost near!!')
 else:
     print("your Chance is over")
+    print('The number is :',a)
